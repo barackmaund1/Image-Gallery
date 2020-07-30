8359ea7e96c1681d318422d75a0cee5dc6ecc16a
barackmaund1 <pmaundu49@gmail.co>
2020-07-30 13:32:29 +0300

    Updates

---
+++ precache-manifest.ff29c5e68b29555eade3de1a5053020e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "041f0f12fdccfb395b8ead2489557dc9",
+    "url": "/Image-Gallery/index.html"
+  },
+  {
+    "revision": "f1f07537b535bc0e450f",
+    "url": "/Image-Gallery/static/css/main.ed28c89c.chunk.css"
+  },
+  {
+    "revision": "84b3e8c4a0dca1e5a564",
+    "url": "/Image-Gallery/static/js/2.481918a7.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Image-Gallery/static/js/2.481918a7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f1f07537b535bc0e450f",
+    "url": "/Image-Gallery/static/js/main.5b68303f.chunk.js"
+  },
+  {
+    "revision": "f52ef6e9c9f4694d20b3",
+    "url": "/Image-Gallery/static/js/runtime-main.bfc7da17.js"
+  }
+]);
